82ae71fb04942f3429b683db7f2b5a4730d7cad6
otto-wakarimasen <63456539+otto-wakarimasen@users.noreply.github.com>
2024-06-19 20:43:19 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { BlogContent } from "./components/BlogContent/BlogContent";
+import { BlogContent } from "./containers/BlogContent/BlogContent";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import "./App.css"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div>
 <Header />
     <main>
-      <BlogContent />
+     <Routes>
+        <Route path="/" element={<BlogContent />} />
+     </Routes>
     </main>
       <Footer year={new Date().getFullYear()}/>
     </div>
+    </Router>
   );
 }
 
