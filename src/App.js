58cbc29e4eaa3a2d8fd25c13ad46c3bdd49a3d4a
otto-wakarimasen <63456539+otto-wakarimasen@users.noreply.github.com>
2024-06-19 21:27:25 +0300

    27start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BlogContent } from "./containers/BlogContent/BlogContent";
+import { BlogContent } from "./containers/BlogPage/BlogContent";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import "./App.css"
